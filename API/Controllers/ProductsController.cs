2559bfa0edab2a5c71415adf5237bf3561f445d5
Hemanth Peethani <hemanthpeethani9@gmail.com>
2022-03-09 11:36:05 +0530

    End of section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,30 +2,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
-    {
-        private readonly IGenericRepository<Product>_productsRepo;
-         private readonly IGenericRepository<ProductBrand>_productBrandRepo;
-          private readonly IGenericRepository<ProductType>_productTypeRepo;
-          private readonly IMapper _mapper;
 
-        public ProductsController(IGenericRepository<Product> productsRepo, 
-        IGenericRepository<ProductBrand>productBrandRepo, IGenericRepository<ProductType>
-        productTypeRepo,IMapper mapper)
+    public class ProductsController : BaseApiController
+    {
+        private readonly IGenericRepository<Product> _productsRepo;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepo;
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IGenericRepository<Product> productsRepo,
+        IGenericRepository<ProductBrand> productBrandRepo, IGenericRepository<ProductType>
+        productTypeRepo, IMapper mapper)
         {
-             _mapper = mapper;
+            _mapper = mapper;
             _productTypeRepo = productTypeRepo;
             _productBrandRepo = productBrandRepo;
             _productsRepo = productsRepo;
@@ -34,30 +35,35 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<ProductToReturnDto>>> GetProducts()
         {
-            var spec  = new ProductsWithTypesAndBrandsSpecification();
-            var products = await  _productsRepo.ListAsync(spec);
-            return  Ok(_mapper.Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>
+            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var products = await _productsRepo.ListAsync(spec);
+            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>
             (products));
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
-            var spec  = new ProductsWithTypesAndBrandsSpecification(id);
-            var product =  await _productsRepo.GetEntityWithSpec(spec);
-            return _mapper.Map<Product, ProductToReturnDto>(product); 
+            var spec = new ProductsWithTypesAndBrandsSpecification(id);
+            var product = await _productsRepo.GetEntityWithSpec(spec);
+            if (product == null) return NotFound(new ApiResponse(404));
+
+            return _mapper.Map<Product, ProductToReturnDto>(product);
         }
 
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
-            return Ok(await _productBrandRepo.ListAllAsync()); 
+            return Ok(await _productBrandRepo.ListAllAsync());
         }
 
-       [HttpGet("types")]
+        [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
         {
-            return Ok(await _productTypeRepo.ListAllAsync()); 
+            return Ok(await _productTypeRepo.ListAllAsync());
         }
 
 
