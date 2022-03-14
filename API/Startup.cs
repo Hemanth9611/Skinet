eb085aaccba202da8fc2075589f77f89180cb9bb
Hemanth Peethani <hemanthpeethani9@gmail.com>
2022-03-14 15:24:05 +0530

    Angular Basics--->End of Section 8

--- API/Startup.cs
+++ API/Startup.cs
@@ -41,6 +41,8 @@ namespace API
             services.AddCors(opt => {
                 opt.AddPolicy("CorsPolicy", policy =>{
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+
                 });
             });
            
